fb1b689099653e9c67bc031eda7382df8633cfb1
Hashrocket Workstation <dev@hashrocket.com>
2017-02-27 16:07:18 -0500

    Authenticate admin on certain actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_admin, only: [:new, :edit]
   expose :post
   expose :posts do
     Post.all
@@ -18,10 +19,21 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.user_id = current_user.id
+
+    if params[:publish_date].blank?
+      post.publish_date = Date.today
+    end
+
     if post.save
       redirect_to post_path(post)
     else
       render :new
+    end
+  end
+
+  def authenticate_admin
+    if current_user.nil? || !current_user.is_admin?
+      redirect_to root_path
     end
   end
 
