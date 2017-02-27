dff60201fef64ba807ea9b659d8a13f8809ed9ee
Hashrocket Workstation <dev@hashrocket.com>
2017-02-27 00:24:42 -0500

    Basic styles

---
+++ db/migrate/20170227050050_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.boolean :is_admin
+      t.string :email,                null: false
+      t.string :password_digest,      null: false
+      t.string :password_reset_token, null: false, limit: 60
+
+      t.timestamps
+    end
+  end
+end
