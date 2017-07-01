b31f3b3a4e4627d7772d37de3ad9721c54aa8ba7
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-07-01 15:47:36 -0400

    Change datatype of from string to text on course

---
+++ db/migrate/20170701194539_change_string_to_text_courses.rb
@@ -0,0 +1,5 @@
+class ChangeStringToTextCourses < ActiveRecord::Migration
+  def change
+    change_column :courses, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402230103) do
+ActiveRecord::Schema.define(version: 20170701194539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20170402230103) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "course_code"
-    t.string   "description"
+    t.text     "description"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
