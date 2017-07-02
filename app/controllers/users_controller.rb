048975b119da5dfe3801cbdf0ecca5ef9f94bbf7
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-07-02 18:55:19 -0400

    Refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,8 +58,8 @@ class UsersController < ApplicationController
   private
 
   def user_authorize
-    unless current_user == user || current_user.is_admin?
-      redirect_to root_path
+    unless current_user.present? && (current_user == user || current_user.admin?)
+      redirect_to root_path(message: 'authority_issue')
     end
   end
 
